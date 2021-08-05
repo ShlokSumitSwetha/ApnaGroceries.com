a837615124acbc8ebd9eb9ec29bc662798254854
Sumith Ksherasagar <ksherasagar@gmail.com>
2021-08-05 23:12:31 +0100

    Initial commit

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyDpQLjcDxfACtdfgc3Jh0RzxsYCgnu3r7c",
+  authDomain: "shopping-cart-a7826.firebaseapp.com",
+  databaseURL: "https://shopping-cart-a7826-default-rtdb.firebaseio.com/",
+  projectId: "shopping-cart-a7826",
+  storageBucket: "gs://shopping-cart-a7826.appspot.com",
+  messagingSenderId: "<YOUR_MESSAGING_SENDER_ID>",
+};
